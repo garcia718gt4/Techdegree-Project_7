47146c32ec8d108572c2067390b75c3358aace3d
garcia718gt4 <58898368+garcia718gt4@users.noreply.github.com>
2020-06-15 02:35:52 -0700

    Final version 1.0

--- js/main.js
+++ js/main.js
@@ -264,7 +264,25 @@ const mobileOptions = {
 
 
 /* ======================================
+    Messaging Section: 
     Create variables to store the form fields
    =======================================  */
-
    
+   const user = document.getElementById('userField'); 
+   const message = document.getElementById('messageField');
+   const send = document.getElementById('send');
+
+   /* Create a click event listener on send */
+   send.addEventListener('click', () => {
+
+        // ensure user and message fields are filled out
+        if (user.value === '' && message.value === '') {
+            alert('Please fill out user and message fields before sending');
+        } else if (user.value === '') {
+            alert('Please fill out the user field before sending');
+        } else if (message.value === '') {
+            alert('Please fill out message field before sending');
+        } else {
+            alert(`Message successfully sent to: ${user.value}`);
+        }
+   }); 
